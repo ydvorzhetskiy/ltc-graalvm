6a66ce896d27f27651618e25226cce896c27be5a
ydvorzhetskiy <dvorzhetskij@mail.ru>
2020-12-01 16:39:06 +0600

    NodeJS example

---
+++ hello-word-nodejs/index.js
@@ -0,0 +1,13 @@
+// Set up PATH according to
+// https://www.graalvm.org/docs/getting-started-with-graalvm/windows/
+// or
+// https://www.graalvm.org/docs/getting-started-with-graalvm/linux/
+//
+
+// node index.js
+// or
+// npm start
+
+const server = () => console.log('Hello from NodeJS');
+
+server();
