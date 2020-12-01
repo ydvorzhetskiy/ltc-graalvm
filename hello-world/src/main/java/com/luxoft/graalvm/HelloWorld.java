b34add2e8eaefceae851143b59c5bdd7d0fd74ed
ydvorzhetskiy <dvorzhetskij@mail.ru>
2020-12-01 13:41:23 +0600

    Hello world

---
+++ hello-world/src/main/java/com/luxoft/graalvm/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.luxoft.graalvm;
+
+class HelloWorld {
+
+    // Set up PATH and JAVA_HOME according to
+    // https://www.graalvm.org/docs/getting-started-with-graalvm/windows/
+    // or
+    // https://www.graalvm.org/docs/getting-started-with-graalvm/linux/
+    //
+    // javac src/main/java/com/luxoft/graalvm/HelloWorld.java
+    // cd src/main/java
+    // java com.luxoft.graalvm.HelloWorld
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
