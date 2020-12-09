c4a7d9054e84fe180e266112bbabe4ace16b3e47
ydvorzhetskiy <dvorzhetskij@mail.ru>
2020-12-10 05:38:52 +0600

    Python example

---
+++ hello-world-python/hello.py
@@ -0,0 +1,10 @@
+# GraalVM 20.3.0: python available only linux
+
+# Set up PATH and JAVA_HOME according to
+# https://www.graalvm.org/docs/getting-started-with-graalvm/linux/
+# gu install python
+#
+# graalpython hello.py
+
+name = "Ivan"
+print("Hello, ", name)
